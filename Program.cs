fcbe962e2b9782fd7178d58735eda6393a4f9400
Chris Schnurr <ctschnurr@hotmail.com>
2023-10-12 14:36:21 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSavingPractice
+{
+   
+    internal class Program
+    {
+        // player constants
+        const int playerMaxHealth = 20;
+
+        static void Main(string[] args)
+        {
+            if (!System.IO.File.Exists("globals.txt"))
+            {
+                System.IO.File.Create(@"globals.txt").Close();
+                // System.IO.File.WriteAllText(@"savegame.txt", savePage);
+            }
+
+        }
+    }
+
+}
